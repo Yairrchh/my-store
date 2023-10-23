098aba3f6e1e261d7f3b00e670d29d24306af186
unknown <chirayair@gmail.com>
2023-10-23 12:35:36 -0400

    two commit

--- index.js
+++ index.js
@@ -8,19 +8,19 @@ const {logError, errorHandle, boomErrorHandle} = require("./middlewares/errorHan
 const app = express();
 const port = 3005;
 
-const whiteList = ['http://localHost:8080', 'http://127.0.0.1:5500']
-const optionsCors = {
-  origin: (origin, callback) => {
-    if(whiteList.includes(origin)) {
-      callback(null,true);
-    } else {
-      callback(new Error('No access'))
-    }
-  }
-};
+// const whiteList = ['http://localHost:8080', 'http://127.0.0.1:5500']
+// const optionsCors = {
+//   origin: (origin, callback) => {
+//     if(whiteList.includes(origin)) {
+//       callback(null,true);
+//     } else {
+//       callback(new Error('No access'))
+//     }
+//   }
+// };
 
 app.use(express.json());
-app.use(cors(optionsCors));
+app.use(cors());
 
 app.get('/', (req, res) => {
   res.send('Hello this is my serve to express')
